21b3cae37820153ae4ff0c65d18a90ee29812fa0
Harold Torres Marino <haroldtm55@gmail.com>
2021-07-17 16:42:40 +1000

    Code-along completed

---
+++ src/Signup.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Signup = () => 
+  <form>
+  <h1>Signup</h1>
+  <div>
+    <input type="text" name="name" placeholder="Name" />
+    <label htmlFor="username">Name</label>
+  </div>
+  <div>
+    <input type="text" name="username" placeholder="Username" />
+    <label htmlFor="username">Username</label>
+  </div>
+  <div>
+    <input type="password" name="password" placeholder="Password" />
+    <label htmlFor="password">Password</label>
+  </div>
+  <input type="submit" value="Signup" />
+  </form>;
+
+  export default Signup
